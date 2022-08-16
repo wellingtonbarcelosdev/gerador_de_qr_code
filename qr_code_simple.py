fc933e9724841df1d274978f82ee361477377319
WELLINGTON BARCELOS <wellingtonbarcelosdev@gmail.com>
2022-08-15 22:31:13 -0300

    qrcode color

--- qr_code_simple.py
+++ qr_code_simple.py
@@ -1,7 +1,4 @@
 import qrcode
-
-data = 'https://www.linkedin.com/in/wellingtonbarcelos'
-
-img = qrcode.make(data)
-
+link = 'https://www.linkedin.com/in/wellingtonbarcelos'
+img = qrcode.make(link)
 img.save('C:/Users/vitor/OneDrive/Documentos/WELLINGTON/Programação/python/Projetos/código qr_code/myqrcode.png')
