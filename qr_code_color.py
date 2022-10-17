ec3be818796c736e5bda9911ea4daf4eacce704c
WELLINGTON BARCELOS <wellingtonbarcelosdev@gmail.com>
2022-10-17 20:01:28 -0300

    ajuste link

--- qr_code_color.py
+++ qr_code_color.py
@@ -1,8 +1,8 @@
 from textwrap import fill
 import qrcode
-link = 'https://www.linkedin.com/in/wellingtonbarcelos'
+link = 'https://github.com/wellingtonbarcelosdev'
 qr = qrcode.QRCode(version = 1, box_size = 10, border=5)
 qr.add_data(link)
 qr.make(fit=True)
-img = qr.make_image(fill_color = 'yellow', back_color = 'black')
+img = qr.make_image(fill_color = 'black', back_color = 'white')
 img.save('C:/Users/vitor/OneDrive/Documentos/WELLINGTON/Programação/python/Projetos/código qr_code/myqrcode1.png')
--- qr_code_simple.py
+++ qr_code_simple.py
@@ -1,4 +1,4 @@
 import qrcode
-link = 'https://www.linkedin.com/in/wellingtonbarcelos'
+link = 'https://github.com/wellingtonbarcelosdevhttps://github.com/wellingtonbarcelosdev'
 img = qrcode.make(link)
 img.save('C:/Users/vitor/OneDrive/Documentos/WELLINGTON/Programação/python/Projetos/código qr_code/myqrcode.png')
